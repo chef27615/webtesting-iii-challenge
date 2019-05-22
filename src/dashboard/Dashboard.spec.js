0e8ef9243998a6bff766c5959eb630b0ed49092c
wei du <weidu@weis-Mac-mini.local>
2019-05-22 18:24:24 -0400

    tested in all modules

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,27 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+    
+    it('should render without crash', () => {
+        render(<Dashboard />)
+        cleanup();
+    })
+
+    it('should match a snapshot', () => {
+        const tree = renderer.create(<Dashboard />);
+        expect(tree.toJSON()).toMatchSnapshot();
+        cleanup()
+    })
+
+    it('should control a mocked toggle',  () => {
+       const toggle = jest.fn(() => true);
+       toggle();
+       expect(toggle).toBeCalled()
+    })
+    
+
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,32 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import Display from './Display';
+
+const receivedData = {
+    closed: true,
+    locked: false
+}
+
+describe('<Display />', () => {
+    it('should render', () => {
+        render(<Display />);
+        cleanup()
+    })
+
+    it('should received closed as props', () => {
+        const { closed } = receivedData;
+        const { getByText } = render(<Display closed={closed} />)
+        const text = getByText(/closed/i);
+        expect(text).toBeTruthy()
+        cleanup()
+    })
+    
+    it('should received locked as props', () => {
+        const { locked } = receivedData;
+        const { getByText } = render(<Display locked={locked} />)
+        const text = getByText(/locked/i);
+        expect(text).toBeTruthy()
+        cleanup()
+    })
+
+})
